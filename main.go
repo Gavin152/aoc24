3d99455bec25f67dea2e36f1957b206f9324da37
niko <nikolas.molinari@gmail.com>
2024-12-02 23:34:42 +0100

    work in progress

--- main.go
+++ main.go
@@ -8,33 +8,24 @@ import (
 	"github.com/Gavin152/aoc24/internal/filereader"
 )
 
-func isSafe(report []string) bool {
-	previous, _ := strconv.Atoi(report[0])
-	level, _ := strconv.Atoi(report[1])
+func remove(slice []int, s int) []int {
+	return append(slice[:s], slice[s+1:]...)
+}
 
+func isSafe(report []int, retry bool) bool {
+	previous := report[0]
+	level := report[1]
 	rising := true
+
 	if previous > level {
 		rising = false
 	}
 
-	for i, string_val := range report {
+	for i, val := range report {
 		if i == 0 {
 			continue
 		}
-		level, _ = strconv.Atoi(string_val)
-
-		// if level > previous && (level-previous < 1 || level-previous > 3) {
-		// 	fmt.Printf("Round: %d | Distance: %d\n", i+1, level-previous)
-		// 	return false
-		// }
-		// if previous > level && (previous-level < 1 || previous-level > 3) {
-		// 	fmt.Printf("Round: %d | Distance: %d\n", i+1, previous-level)
-		// 	return false
-		// }
-		// if level == previous {
-		// 	fmt.Printf("Round: %d | Distance: %d\n", i+1, level-previous)
-		// 	return false
-		// }
+		level = val
 
 		distance := level - previous
 		if !rising {
@@ -42,11 +33,11 @@ func isSafe(report []string) bool {
 		}
 
 		if rising && level < previous {
-			return false
+
 		}
 
 		if distance < 1 || distance > 3 {
-			return false
+
 		}
 
 		previous = level
@@ -59,15 +50,22 @@ func main() {
 
 	safeCount := 0
 
-	// filePath := "example.txt"
-	filePath := "data_a"
+	filePath := "example.txt"
+	// filePath := "data_a"
 	// filePath := "data_b"
 
 	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
 		clean_line := s.TrimSpace(line)
 		split_line := s.Split(clean_line, " ")
 
-		if isSafe(split_line) {
+		reps := []int{}
+		for _, val := range split_line {
+			num, _ := strconv.Atoi(val)
+			reps = append(reps, num)
+		}
+
+		if isSafe(reps, true) {
+			// fmt.Printf("Safe report found: %v\n", split_line)
 			safeCount++
 		}
 
