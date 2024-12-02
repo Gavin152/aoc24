73e34887b8c9c5f5dff31d491b90e06927e8c70d
niko <nikolas.molinari@gmail.com>
2024-12-02 20:26:47 +0100

    solved

--- main.go
+++ main.go
@@ -2,20 +2,53 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	s "strings"
 
 	"github.com/Gavin152/aoc24/internal/filereader"
 )
 
 func main() {
-	// Example usage
-	filePath := "example.txt"
-	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
-		// Custom processing logic for each line
-		fmt.Println(line)
-		return nil // Return an error if processing fails
-	})
 
+	left := []int{}
+	right := []int{}
+	tally := 0
+
+	// filePath := "example.txt"
+	filePath := "data_a"
+	// filePath := "data_b"
+
+	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
+		clean_line := s.TrimSpace(line)
+		split_line := s.Split(clean_line, " ")
+
+		left_number, l_err := strconv.Atoi(split_line[0])
+		right_number, r_err := strconv.Atoi(split_line[len(split_line)-1])
+
+		if l_err != nil || r_err != nil {
+			fmt.Printf("Error parsing line: \nLEFT: %v\n RIGHT:%v\n", l_err, r_err)
+		}
+
+		left = append(left, left_number)
+		right = append(right, right_number)
+		return nil
+	})
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	for i, _ := range left {
+		// fmt.Printf("%d", left[i])
+		if left[i] >= right[i] {
+			tally += left[i] - right[i]
+		} else if left[i] < right[i] {
+			tally += right[i] - left[i]
+		}
+	}
+
+	fmt.Printf("Final tally is %d", tally)
 }
