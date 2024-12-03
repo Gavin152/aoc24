b1b7d5946f3ed75cc703a8c4865c4d9744c91727
niko <nikolas.molinari@gmail.com>
2024-12-03 23:13:12 +0100

    guess the guess

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func remove(slice []int, s int) []int {
 	return append(slice[:s], slice[s+1:]...)
 }
 
-func isSafe(report []int, retry bool) bool {
+func findInvalidIndex(report []int) int {
 	previous := report[0]
 	level := report[1]
 	rising := true
@@ -28,30 +28,56 @@ func isSafe(report []int, retry bool) bool {
 		level = val
 
 		distance := level - previous
-		if !rising {
+		if distance < 0 {
 			distance = distance * -1
 		}
 
-		if rising && level < previous {
-
+		isSequential := (rising && level > previous) || (!rising && previous > level)
+		isValid := 1 <= distance && distance <= 3
+		if !isSequential || !isValid {
+			return i
 		}
-
-		if distance < 1 || distance > 3 {
-
-		}
-
 		previous = level
 	}
-	fmt.Printf("Safe report found: %v\n", report)
-	return true
+	// fmt.Printf("Safe report found: %v\n", report)
+	return -1
+}
+
+func isSafe(report []int) bool {
+	invalidIndex := findInvalidIndex(report)
+	if invalidIndex < 0 {
+		fmt.Printf("Valid report: %v\n", report)
+		return true
+	}
+	index1, index2 := invalidIndex-1, invalidIndex
+	swappedReport1 := deleteLevelAt(report, index1)
+	swappedReport2 := deleteLevelAt(report, index2)
+	swap1 := findInvalidIndex(swappedReport1) == -1
+	swap2 := findInvalidIndex(swappedReport2) == -1
+	if swap1 {
+		fmt.Printf("valid on retry %v\n", swappedReport1)
+		return true
+	}
+	if swap2 {
+		fmt.Printf("valid on retry %v\n", swappedReport2)
+		return true
+	}
+	return false
+}
+
+func deleteLevelAt(levels []int, idx int) []int {
+	deleted := make([]int, len(levels)-1)
+	copy(deleted[:idx], levels[:idx])
+	copy(deleted[idx:], levels[idx+1:])
+	return deleted
 }
 
 func main() {
 
 	safeCount := 0
 
-	filePath := "example.txt"
-	// filePath := "data_a"
+	// filePath := "example.txt"
+	filePath := "data_a"
 	// filePath := "data_b"
 
 	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
@@ -64,7 +90,7 @@ func main() {
 			reps = append(reps, num)
 		}
 
-		if isSafe(reps, true) {
+		if isSafe(reps) {
 			// fmt.Printf("Safe report found: %v\n", split_line)
 			safeCount++
 		}
