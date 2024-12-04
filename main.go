6954a53f25ed0db19d95d4146cb56e197e693bd5
niko <nikolas.molinari@gmail.com>
2024-12-04 19:25:07 +0100

    worky worky

--- main.go
+++ main.go
@@ -16,11 +16,17 @@ func findWords() int {
 		for j, rawChar := range line {
 			char := string(rawChar)
 
-			if char == "X" {
+			if char == "A" {
+				masCnt := 0
+				skip := false
 				for _, vec := range directions {
 					if checkDirection([]int{i, j}, vec) {
+						masCnt++
+					}
+					if masCnt == 2 && !skip {
+						fmt.Printf("Valid cross: [%d,%d]\n", i+1, j+1)
 						xmasCounter++
-						fmt.Printf("COUNTER: %d\n", xmasCounter)
+						skip = true
 					}
 				}
 			}
@@ -31,40 +37,36 @@ func findWords() int {
 }
 
 func checkDirection(anchor []int, vector []int) bool {
-	fmt.Printf("checking X in [%d,%d] in dir %v\n", anchor[0], anchor[1], vector)
-	var h int
-	var v int
-	for i := 1; i < 4; i++ {
-		h = anchor[1] + vector[1]*i
-		v = anchor[0] + vector[0]*i
-		if !isInBounds([]int{v, h}) {
-			return false
+	// fmt.Printf("checking X in [%d,%d] in dir %v\n", anchor[0], anchor[1], vector)
+	// var h int
+	// var v int
+	h := anchor[1] + vector[1]
+	v := anchor[0] + vector[0]
+
+	hinv := anchor[1] + vector[1]*-1
+	vinv := anchor[0] + vector[0]*-1
+
+	if !isInBounds([]int{v, h}) || !isInBounds([]int{vinv, hinv}) {
+		return false
+	} else {
+		char := string(lines[v][h])
+		charinv := string(lines[vinv][hinv])
+		if char == "M" && charinv == "S" {
+			return true
 		} else {
-			char := string(lines[v][h])
-			// fmt.Printf("i: %d ", i)
-			// fmt.Printf("| char: %s\n", char)
-			if i == 1 && char == "M" {
-				continue
-			} else if i == 2 && char == "A" {
-				continue
-			} else if i == 3 && char == "S" {
-				continue
-			} else {
-				return false
-			}
+			return false
 		}
 	}
-	return true
 }
 
 func isInBounds(pos []int) bool {
 	// fmt.Printf("Checking [%d,%d]\n", pos[1], pos[0])
 	if pos[0] < 0 || pos[0] > len(lines[0])-1 {
-		fmt.Printf("[%d,%d] is out of bounds\n", pos[1], pos[0])
+		// fmt.Printf("[%d,%d] is out of bounds\n", pos[1], pos[0])
 		return false
 	}
 	if pos[1] < 0 || pos[1] > len(lines)-1 {
-		fmt.Printf("[%d,%d] is out of bounds\n", pos[1], pos[0])
+		// fmt.Printf("[%d,%d] is out of bounds\n", pos[1], pos[0])
 		return false
 	}
 	return true
@@ -75,14 +77,10 @@ func main() {
 	// safeCount := 0
 	lines = []string{}
 	directions = [][]int{
-		{-1, 0},  // up
 		{-1, 1},  // up right
-		{0, 1},   // right
 		{1, 1},   // btm right
-		{1, 0},   // btm
 		{1, -1},  // btm left
-		{0, -1},  // left
-		{-1, -1}, // up
+		{-1, -1}, // up left
 	}
 
 	// filePath := "example.txt"
