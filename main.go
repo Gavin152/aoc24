5347844691119b88aa356bb2674d0912bb0b733d
niko <nikolas.molinari@gmail.com>
2024-12-05 16:14:19 +0100

    workin

--- main.go
+++ main.go
@@ -33,25 +33,44 @@ func splitUpdates(raw *[]string, split *[][]int) {
 func checkUpdates(updates *[][]int, rules *[][]int) int {
 	sum := 0
 	for _, update := range *updates {
-		isValid := true
-		for i, rule := range *rules {
-			fmt.Printf("Checking Rule %d\n", i)
-			u_index1 := slices.Index(update, rule[0])
-			u_index2 := slices.Index(update, rule[1])
-			if u_index1 == -1 || u_index2 == -1 {
-				continue
-			}
-			if !(u_index1 < u_index2) {
-				fmt.Printf("Break on %d\n", i)
-				isValid = false
+		isValid, breaking_rule := checkUpdate(&update, rules)
+		for !isValid {
+			reordered := reorder(&update, breaking_rule)
+			isValid, breaking_rule = checkUpdate(&reordered, rules)
+			if isValid {
+				sum += findCenter(&reordered)
 				break
 			}
 		}
-		if isValid {
-			sum += findCenter(&update)
-		}
 	}
 	return sum
+}
+
+func checkUpdate(update *[]int, rules *[][]int) (bool, []int) {
+	for _, rule := range *rules {
+		u_index1 := slices.Index(*update, rule[0])
+		u_index2 := slices.Index(*update, rule[1])
+		if u_index1 == -1 || u_index2 == -1 {
+			continue
+		}
+		if !(u_index1 < u_index2) {
+			return false, rule
+		}
+	}
+	return true, nil
+}
+
+func reorder(update *[]int, breaking_rule []int) []int {
+	// fmt.Printf("Broken Update: %v\n", update)
+	// fmt.Printf("Broken by rule: %v\n", breaking_rule)
+	idx := slices.Index(*update, breaking_rule[1])
+	tmp_update := slices.Delete(*update, idx, idx+1)
+	tmp_update = slices.Insert(tmp_update, len(tmp_update), breaking_rule[1])
+	// fmt.Printf("Temp Update: %v\n", tmp_update)
+
+	// fmt.Println("\n==============================")
+
+	return tmp_update
 }
 
 func findCenter(update *[]int) int {
