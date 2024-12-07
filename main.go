2038d71226f9a807093bf3b7fca94725107f2f85
niko <nikolas.molinari@gmail.com>
2024-12-07 01:17:43 +0100

    fiiiiiinally

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"slices"
+	"strconv"
 
 	"github.com/Gavin152/aoc24/internal/filereader"
 )
@@ -10,16 +11,18 @@ import (
 var directions map[rune][]int
 var startPoint []int
 var startDirection rune
+var lab [][]rune
+var visited map[string][]rune
 
-func fillMatrix(lines []string) [][]rune {
+func fillMatrix(lines []string) {
 	xlen := len(lines[0])
 	ylen := len(lines)
-	lab := make([][]rune, xlen)
+	lab = make([][]rune, xlen)
 	for i, _ := range lab {
 		lab[i] = make([]rune, ylen)
 	}
 
-	fmt.Printf("lab length: %d, lab element length: %d\n", len(lab), len(lab[0]))
+	// fmt.Printf("lab length: %d, lab element length: %d\n", len(lab), len(lab[0]))
 	for i, line := range lines {
 		col := []rune(line)
 		for j, character := range col {
@@ -31,15 +34,18 @@ func fillMatrix(lines []string) [][]rune {
 			}
 		}
 	}
-	return lab
 }
 
-func walk(lab [][]rune) {
+func walk() bool {
 	current := startPoint
 	direction := startDirection
-	fmt.Printf("Startpoint is %d|%d, direction %c\n", current[0], current[1], direction)
+	// fmt.Printf("Startpoint is %d|%d, direction %c\n", current[0], current[1], direction)
 	for {
 		lab[current[0]][current[1]] = 'X'
+		isLoop := setMarker(current, direction)
+		if isLoop {
+			return true
+		}
 		next := []int{
 			current[0] + directions[direction][0],
 			current[1] + directions[direction][1],
@@ -47,12 +53,24 @@ func walk(lab [][]rune) {
 		if isOutOfBounds(lab, next) {
 			break
 		}
-		if lab[next[0]][next[1]] == '#' {
+		if lab[next[0]][next[1]] == '#' || lab[next[0]][next[1]] == 'O' {
 			direction = turn(direction)
 			continue
 		}
 		current = next
 	}
+	return false
+}
+
+func setMarker(pos []int, direction rune) bool {
+	ps := strconv.Itoa(pos[0]) + "|" + strconv.Itoa(pos[1])
+	dirs, exists := visited[ps]
+	if exists && slices.Index(dirs, direction) > -1 {
+		fmt.Printf("Already travelled this field in this direction\n")
+		return true
+	}
+	visited[ps] = append(visited[ps], direction)
+	return false
 }
 
 func turn(current rune) rune {
@@ -82,19 +100,19 @@ func isOutOfBounds(lab [][]rune, pos []int) bool {
 	return false
 }
 
-func tallyPositions(lab [][]rune) int {
-	tally := 0
+func tallyPositions() [][]int {
+	tally := [][]int{}
 	for i, col := range lab {
 		for j, _ := range col {
 			if lab[i][j] == 'X' {
-				tally++
+				tally = append(tally, []int{i, j})
 			}
 		}
 	}
 	return tally
 }
 
-func printLab(lab [][]rune) {
+func printLab() {
 	for i, col := range lab {
 		for j, _ := range col {
 			fmt.Printf("%s", string(lab[j][i]))
@@ -112,9 +130,13 @@ func main() {
 		'v': []int{0, 1},
 		'<': []int{-1, 0},
 	}
+	visited = map[string][]rune{}
+
+	possibleObs := [][]int{}
+	confirmedObs := [][]int{}
+
 	// filePath := "example.txt"
-	filePath := "data_a"
-	// filePath := "data_b"
+	filePath := "data"
 
 	lines := []string{}
 
@@ -126,12 +148,30 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 	}
 
-	lab := fillMatrix(lines)
-	fmt.Printf("%s", string(lab[4]))
-	walk(lab)
+	fillMatrix(lines)
+	walk()
+	possibleObs = tallyPositions()
 
-	total := tallyPositions(lab)
-	printLab(lab)
+	for _, pos := range possibleObs {
+		fillMatrix(lines)
+		for k := range visited {
+			delete(visited, k)
+		}
+		lab[pos[0]][pos[1]] = 'O'
+		isLooped := walk()
+		if isLooped {
+			confirmedObs = append(confirmedObs, pos)
+		}
+		lab[pos[0]][pos[1]] = 'X'
+	}
+
+	total := tallyPositions()
+	printLab()
 
 	fmt.Printf("The guard visited %d positions\n", total)
+	fmt.Printf("The guard visited %d positions\n", len(visited))
+
+	fmt.Printf("Possible Obstruction Positions %d\n", len(possibleObs))
+	fmt.Printf("Confirmed Obstruction Positions %d\n", len(confirmedObs))
+
 }
