062f0d6625e3e9fc13e7a1664abd0347992de48d
niko <nikolas.molinari@gmail.com>
2024-12-11 23:05:21 +0100

    this was a fucking pain, man

--- main.go
+++ main.go
@@ -70,6 +70,86 @@ func defrag(line []int) []int {
 	return cleaned
 }
 
+func getFreeBlocks(line []int) [][]int {
+	free_blocks := [][]int{}
+	count_free := 0
+	index_free := -1
+	for i, block := range line {
+
+		// fmt.Printf("Block: %d at index %d\n", block, i)
+		if block == -1 {
+			count_free++
+			if index_free == -1 {
+				index_free = i
+			}
+		} else if count_free > 0 {
+			free_blocks = append(free_blocks, []int{index_free, count_free})
+			count_free = 0
+			index_free = -1
+		}
+	}
+	// fmt.Printf("Free blocks: %v\n", free_blocks)
+	return free_blocks
+}
+
+func reshuffle(line []int) []int {
+	free_blocks := getFreeBlocks(line)
+
+	file_blocks := [][]int{}
+	tmp_block := []int{}
+
+	block_id := -1
+	// block_length := 0
+	for i := len(line) - 1; i >= 0; i-- {
+		if line[i] >= 0 {
+			if block_id == -1 {
+				block_id = line[i]
+			}
+			if line[i] == block_id {
+				tmp_block = append(tmp_block, line[i])
+			} else {
+				file_blocks = append(file_blocks, tmp_block)
+				if line[i] != -1 {
+					block_id = line[i]
+					tmp_block = []int{block_id}
+				} else {
+					tmp_block = []int{}
+				}
+			}
+		}
+	}
+	// fmt.Printf("File blocks: %v\n", file_blocks)
+
+	for _, fb := range file_blocks {
+		// fmt.Printf("File block: %v\n", fb)
+		dest_index := slices.IndexFunc(free_blocks, func(free_block []int) bool {
+			return free_block[1] >= len(fb)
+		})
+		if dest_index != -1 {
+			dest_block := free_blocks[dest_index]
+			if dest_block[0] >= slices.Index(line, fb[0]) {
+				continue
+			}
+			for slices.Index(line, fb[0]) != -1 {
+				idx := slices.Index(line, fb[0])
+				line[idx] = -1
+			}
+
+			
+			for j := 0; j < len(fb); j++ {
+				// fmt.Printf("Moving %d to index %d\n", fb[0], dest_block[0]+j)
+				line[dest_block[0]+j] = fb[0]
+			}
+			free_blocks = getFreeBlocks(line)
+		} else {
+			// fmt.Printf("No free space found for block %v\n", fb)
+		}
+		
+	}
+
+	return line
+}
+
 func calculateChecksum(line []int) int {
 	checksum := 0
 	for i, block := range line {
@@ -88,19 +168,24 @@ func main() {
 
 	checksum := 0
 	unpacked := []int{}
-	defragged := []int{}
+	// defragged := []int{}
 
 	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
 		unpacked = unpack(line)
-		defragged = defrag(unpacked)
-		checksum += calculateChecksum(defragged)
+		// defragged = defrag(unpacked)
+		// checksum += calculateChecksum(defragged)
+
+		// fmt.Printf("Unpacked: %v\n", unpacked)
+		reshuffle(unpacked)
+		// fmt.Printf("Reshuffled: %v\n", unpacked)
+		checksum += calculateChecksum(unpacked)
+
 		return nil
 	})
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 
-	// fmt.Printf("Unpacked: %v\n", unpacked)
 	// fmt.Printf("Defragged: %v\n", defragged)
 	fmt.Printf("Checksum: %d\n", checksum)
 }
