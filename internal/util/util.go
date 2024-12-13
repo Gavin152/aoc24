4f30aff7391191bdb5cb51ec6732d771d71e373d
niko <nikolas.molinari@gmail.com>
2024-12-13 18:29:34 +0100

    first solution using my own utils

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,31 +1,48 @@
 package util
 
-func sliceToMatrix(lines []string) [][]rune {
+import (
+	"fmt"
+	"strconv"
+)
+
+func SliceToGrid (lines []string) [][]rune {
 	xlen := len(lines[0])
 	ylen := len(lines)
-	lab = make([][]rune, xlen)
-	for i, _ := range lab {
-		lab[i] = make([]rune, ylen)
+	grid := make([][]rune, xlen)
+	for i, _ := range grid {
+		grid[i] = make([]rune, ylen)
 	}
 
-	// fmt.Printf("lab length: %d, lab element length: %d\n", len(lab), len(lab[0]))
 	for i, line := range lines {
 		col := []rune(line)
 		for j, character := range col {
-			// fmt.Printf("i: %d, j: %d\n", i, j)
-			lab[j][i] = character
-			if slices.Index([]rune{'^', '>', 'v', '<'}, character) > -1 {
-				startPoint = []int{j, i}
-				startDirection = character
-			}
+			grid[j][i] = character
 		}
 	}
+	return grid
 }
 
-func printLab() {
-	for i, col := range lab {
+func ParseGridToInt (grid [][]rune) ([][]int, error) {
+	gridInt := [][]int{}
+	
+	for i, col := range grid {
+		intcol := []int{}
 		for j, _ := range col {
-			fmt.Printf("%s", string(lab[j][i]))
+			newInt, err := strconv.Atoi(string(grid[j][i]))
+			if err != nil {
+				newInt = -1
+			}
+			intcol = append(intcol, newInt)
+		}
+		gridInt = append(gridInt, intcol)
+	}
+	return gridInt, nil
+}
+
+func PrintGrid [T any](grid [][]T) {
+	for i, col := range grid {
+		for j, _ := range col {
+			fmt.Printf("%v", grid[j][i])
 		}
 		fmt.Printf("\n")
 	}
--- main.go
+++ main.go
@@ -2,163 +2,66 @@ package main
 
 import (
 	"fmt"
-	"slices"
-	"strconv"
-	"strings"
 
 	"github.com/Gavin152/aoc24/internal/filereader"
+	"github.com/Gavin152/aoc24/internal/util"
 )
 
-func unpack(line string) []int {
-	blocks := strings.Split(line, "")
-	blockString := []int{}
-	idx := 0
-	for i, block := range blocks {
-		bump := true
-		blockSize, _ := strconv.Atoi(block)
-		if blockSize == 0 {
-			idx++
-			continue	
-		}
-		for j := 0; j < blockSize; j++ {
-			if i%2 == 0 {
-				blockString = append(blockString, idx)
-			} else {
-				blockString = append(blockString, -1)
-				if bump {
-					idx++
-					bump = false
-				}
+var (
+	trailHeads [][]int
+	grid [][]int
+)	
+
+func getTrailHeads(grid [][]int) [][]int {
+	trailHeads := [][]int{}
+	for i, row := range grid {
+		for j, col := range row {
+			if col == 0 {
+				trailHeads = append(trailHeads, []int{i, j})
 			}
 		}
 	}
-
-	return blockString
+	return trailHeads
 }
 
-func findLastDigit(runes []int, pos int) (digit int, index int) {
-	for i := len(runes) - 1; i >= 0; i-- {
-		if i < pos {
-			break
-		}
-		if runes[i] != -1 {
-			// fmt.Printf("Found digit %c at index %d\n", runes[i], i)
-			return runes[i], i
-		}
-	}
-	return -1, pos
-}
-
-func defrag(line []int) []int {
-	raw_blocks := line
-	new_blocks := line
-	for i := 0; i < len(raw_blocks); i++ {
-		if raw_blocks[i] != -1 {
-			continue
+func hike(pos []int) [][]int {
+	// fmt.Printf("Hiking from %v\n", pos)
+	summits := [][]int{}
+	for _, nextStep := range getNextStep(pos) {
+		if grid[nextStep[0]][nextStep[1]] == 9 {
+			summits = append(summits, nextStep)
 		} else {
-			lastDigit, lastIndex := findLastDigit(raw_blocks, i)
-			if lastDigit != -1 {
-				new_blocks[i] = lastDigit
-				raw_blocks[lastIndex] = -1
-			}
-			new_blocks[lastIndex] = -1
+			summits = append(summits, hike(nextStep)...)
 		}
 	}
-	cleaned := slices.DeleteFunc(new_blocks, func(r int) bool {
-		return r == -1
-	})
-	return cleaned
+	return summits
 }
 
-func getFreeBlocks(line []int) [][]int {
-	free_blocks := [][]int{}
-	count_free := 0
-	index_free := -1
-	for i, block := range line {
-
-		// fmt.Printf("Block: %d at index %d\n", block, i)
-		if block == -1 {
-			count_free++
-			if index_free == -1 {
-				index_free = i
-			}
-		} else if count_free > 0 {
-			free_blocks = append(free_blocks, []int{index_free, count_free})
-			count_free = 0
-			index_free = -1
+func getNextStep(pos []int) [][]int {
+	nextSteps := [][]int{}
+	fmt.Printf("Getting next steps for %v\n", pos)
+	possibleSteps := [][]int{{pos[0]-1, pos[1]}, {pos[0]+1, pos[1]}, {pos[0], pos[1]-1}, {pos[0], pos[1]+1}}
+	
+	filteredSteps := [][]int{}
+	for _, step := range possibleSteps {
+		if isInGrid(step) {
+			filteredSteps = append(filteredSteps, step)
 		}
 	}
-	// fmt.Printf("Free blocks: %v\n", free_blocks)
-	return free_blocks
+	possibleSteps = filteredSteps
+	fmt.Printf("Possible Steps are: %v\n", possibleSteps)
+
+	for _, dir := range possibleSteps {
+		if isInGrid(dir) && grid[dir[0]][dir[1]] == grid[pos[0]][pos[1]] + 1 {
+			fmt.Printf("Hiking from %v to %v\n", pos, dir)
+			nextSteps = append(nextSteps, dir)
+		}
+	}
+	return nextSteps
 }
 
-func reshuffle(line []int) []int {
-	free_blocks := getFreeBlocks(line)
-
-	file_blocks := [][]int{}
-	tmp_block := []int{}
-
-	block_id := -1
-	// block_length := 0
-	for i := len(line) - 1; i >= 0; i-- {
-		if line[i] >= 0 {
-			if block_id == -1 {
-				block_id = line[i]
-			}
-			if line[i] == block_id {
-				tmp_block = append(tmp_block, line[i])
-			} else {
-				file_blocks = append(file_blocks, tmp_block)
-				if line[i] != -1 {
-					block_id = line[i]
-					tmp_block = []int{block_id}
-				} else {
-					tmp_block = []int{}
-				}
-			}
-		}
-	}
-	// fmt.Printf("File blocks: %v\n", file_blocks)
-
-	for _, fb := range file_blocks {
-		// fmt.Printf("File block: %v\n", fb)
-		dest_index := slices.IndexFunc(free_blocks, func(free_block []int) bool {
-			return free_block[1] >= len(fb)
-		})
-		if dest_index != -1 {
-			dest_block := free_blocks[dest_index]
-			if dest_block[0] >= slices.Index(line, fb[0]) {
-				continue
-			}
-			for slices.Index(line, fb[0]) != -1 {
-				idx := slices.Index(line, fb[0])
-				line[idx] = -1
-			}
-
-			
-			for j := 0; j < len(fb); j++ {
-				// fmt.Printf("Moving %d to index %d\n", fb[0], dest_block[0]+j)
-				line[dest_block[0]+j] = fb[0]
-			}
-			free_blocks = getFreeBlocks(line)
-		} else {
-			// fmt.Printf("No free space found for block %v\n", fb)
-		}
-		
-	}
-
-	return line
-}
-
-func calculateChecksum(line []int) int {
-	checksum := 0
-	for i, block := range line {
-		if block == -1 {
-			continue
-		}
-		checksum += block * i
-	}
-	return checksum
+func isInGrid(pos []int) bool {
+	return pos[0] >= 0 && pos[0] < len(grid) && pos[1] >= 0 && pos[1] < len(grid[0])
 }
 
 func main() {
@@ -166,26 +69,35 @@ func main() {
 	// filePath := "example.txt"
 	filePath := "data"
 
-	checksum := 0
-	unpacked := []int{}
-	// defragged := []int{}
+	lines := []string{}
 
 	err := filereader.ReadFileLineByLine(filePath, func(line string) error {
-		unpacked = unpack(line)
-		// defragged = defrag(unpacked)
-		// checksum += calculateChecksum(defragged)
-
-		// fmt.Printf("Unpacked: %v\n", unpacked)
-		reshuffle(unpacked)
-		// fmt.Printf("Reshuffled: %v\n", unpacked)
-		checksum += calculateChecksum(unpacked)
-
+		lines = append(lines, line)
 		return nil
 	})
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
 
-	// fmt.Printf("Defragged: %v\n", defragged)
-	fmt.Printf("Checksum: %d\n", checksum)
+	rawGrid := util.SliceToGrid(lines)
+	grid, _ = util.ParseGridToInt(rawGrid)
+	trailHeads = getTrailHeads(grid)
+
+	fullScore := 0
+	trails := make(map[string	]int)
+	for _, trailHead := range trailHeads {
+		trailSummits := hike(trailHead)
+		trailSummitsMap := make(map[string]bool)	
+		for _, summit := range trailSummits {
+			trailSummitsMap[fmt.Sprintf("%d|%d", summit[0], summit[1])] = true
+		}
+		trails[fmt.Sprintf("%d|%d", trailHead[0], trailHead[1])] = len(trailSummitsMap)
+		fullScore += len(trailSummitsMap)
+		fmt.Printf("Trail Head: %v, Trail Summits: %v\n", trailHead, trailSummitsMap)
+	}
+
+	fmt.Printf("Trail Heads: %v\n", trailHeads)
+	
+	fmt.Printf("Trails: %v\n", trails)
+	fmt.Printf("Full Score: %d\n", fullScore)
 }
