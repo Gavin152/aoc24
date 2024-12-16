bcd4f76527c1e8443e8661fd3b84c0424e374d7f
niko <nikolas.molinari@gmail.com>
2024-12-16 12:44:02 +0100

    what a good night of sleep can do

--- main.go
+++ main.go
@@ -112,28 +112,148 @@ func setArea(region *Region) {
 }
 
 func setBoundaryLength(region *Region) {
-	bLength := 0
+	sides := 0
+
+	for _, plot := range region.Plots {
+		rightNeigh := '.'
+		leftNeigh := '.'
+		upNeigh := '.'
+		downNeigh := '.'
+		lc := []int{plot.Coordinates[0], plot.Coordinates[1] - 1}
+		rc := []int{plot.Coordinates[0], plot.Coordinates[1] + 1}
+		uc := []int{plot.Coordinates[0] - 1, plot.Coordinates[1]}
+		dc := []int{plot.Coordinates[0] + 1, plot.Coordinates[1]}
+		if lc[0] < 0 || lc[1] < 0 || lc[0] >= len(garden) || lc[1] >= len(garden[0]) {
+			// fmt.Printf("Left neighbor is out of bounds\n")
+			leftNeigh = '.'
+		} else {
+			leftNeigh = garden[lc[0]][lc[1]]
+		}
+
+		if rc[0] < 0 || rc[1] < 0 || rc[0] >= len(garden) || rc[1] >= len(garden[0]) {
+			//fmt.Printf("Right neighbor is out of bounds\n")
+			rightNeigh = '.'
+		} else {
+			rightNeigh = garden[rc[0]][rc[1]]
+		}
+
+		if uc[0] < 0 || uc[1] < 0 || uc[0] >= len(garden) || uc[1] >= len(garden[0]) {
+			//fmt.Printf("Up neighbor is out of bounds\n")
+			upNeigh = '.'
+		} else {
+			upNeigh = garden[uc[0]][uc[1]]
+		}
+
+		if dc[0] < 0 || dc[1] < 0 || dc[0] >= len(garden) || dc[1] >= len(garden[0]) {
+			// fmt.Printf("Down neighbor is out of bounds\n")
+			downNeigh = '.'
+		} else {
+			downNeigh = garden[dc[0]][dc[1]]
+		}
+
+		if rightNeigh != region.Identifier && upNeigh != region.Identifier {
+			sides++
+		}
+		if rightNeigh != region.Identifier && downNeigh != region.Identifier {
+			sides++
+		}
+		if leftNeigh != region.Identifier && upNeigh != region.Identifier {
+			sides++
+		}
+		if leftNeigh != region.Identifier && downNeigh != region.Identifier {
+			sides++
+		}
+		sides += checkInnerCorner(plot)
+	}
+	region.BoundaryLength = sides
+}
+
+func getNeighbors(plot Plot) [][]int {
 	directions := [][]int{
 		{0, 1},
 		{1, 0},
 		{0, -1},
 		{-1, 0},
 	}
-	for _, plot := range region.Plots {
-		for _, direction := range directions {
-			if plot.Coordinates[0] + direction[0] < 0 || plot.Coordinates[0] + direction[0] >= len(garden) {
-				bLength++
-				continue
-			} else if plot.Coordinates[1] + direction[1] < 0 || plot.Coordinates[1] + direction[1] >= len(garden[0]) {
-				bLength++
-				continue
-			} else if garden[plot.Coordinates[0] + direction[0]][plot.Coordinates[1] + direction[1]] != region.Identifier {
-				bLength++
-				continue
-			}
+	neighbors := [][]int{}
+	for _, direction := range directions {
+		x := plot.Coordinates[0] + direction[0]
+		y := plot.Coordinates[1] + direction[1]
+		if isInBounds([]int{x, y}) {
+			neighbors = append(neighbors, []int{x, y})
+			fmt.Printf("Neighbor of %c:%v: %c:%v\n", plot.Identifier, plot.Coordinates, garden[x][y], []int{x, y})
 		}
 	}
-	region.BoundaryLength = bLength
+	return neighbors
+}
+
+func checkNeighbor(plot Plot, direction string) bool {
+	var rightNeigh, leftNeigh, upNeigh, downNeigh rune
+
+	if plot.Coordinates[1]+1 >= len(garden[0]) {
+		rightNeigh = '.'
+	} else {
+		rightNeigh = garden[plot.Coordinates[0]][plot.Coordinates[1]+1]
+	}
+	
+	if plot.Coordinates[1]-1 < 0 {
+		leftNeigh = '.'
+	} else {
+		leftNeigh = garden[plot.Coordinates[0]][plot.Coordinates[1]-1]
+	}
+	
+	if plot.Coordinates[0]-1 < 0 {
+		upNeigh = '.'
+	} else {
+		upNeigh = garden[plot.Coordinates[0]-1][plot.Coordinates[1]]
+	}
+	
+	if plot.Coordinates[0]+1 >= len(garden) {
+		downNeigh = '.'
+	} else {
+		downNeigh = garden[plot.Coordinates[0]+1][plot.Coordinates[1]]
+	}
+
+	if rightNeigh == plot.Identifier && upNeigh == plot.Identifier && direction == "topright" {
+		return true
+	}
+	if rightNeigh == plot.Identifier && downNeigh == plot.Identifier && direction == "bottomright" {
+		return true
+	}
+	if leftNeigh == plot.Identifier && upNeigh == plot.Identifier && direction == "topleft" {
+		return true
+	}
+	if leftNeigh == plot.Identifier && downNeigh == plot.Identifier && direction == "bottomleft" {
+		return true
+	}
+	return false
+}
+
+func checkInnerCorner(plot Plot) int {
+	corners := 0
+	directions := map[string][]int{
+		"bottomright": {1, 1},
+		"bottomleft": {1, -1},
+		"topright": {-1, 1},
+		"topleft": {-1, -1},
+	}
+	for name, direction := range directions {
+		x := plot.Coordinates[0] + direction[0]
+		y := plot.Coordinates[1] + direction[1]
+		// fmt.Printf("Checking inner corner at %d|%d\n", x, y)
+		if isInBounds([]int{x, y}) && garden[x][y] != plot.Identifier && checkNeighbor(plot, name) {
+			fmt.Printf("Found inner corner at of %c (%d|%d) %d|%d\n", plot.Identifier, plot.Coordinates[0]+1, plot.Coordinates[1]+1, x+1, y+1)
+			corners++
+		}
+	}
+	return corners
+}
+
+func isInBounds(coordinates []int) bool {
+	if coordinates[0] < 0 || coordinates[0] >= len(garden) || coordinates[1] < 0 || coordinates[1] >= len(garden[0]) {
+		return false
+	}
+	return true
 }
 
 func printRegions() {
@@ -144,8 +264,8 @@ func printRegions() {
 
 func main() {
 
-	// filePath := "example.txt"
 	filePath := "data"
+	// filePath := "example"
 
 	lines := []string{}
 
